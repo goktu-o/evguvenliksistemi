4f0223d799131a49dc6a01aa31c1599490fae67a
goktu-o <92593521+goktu-o@users.noreply.github.com>
2024-12-14 15:10:01 +0300

    firebase veritabanı ayarları yapıldı eklendi. sadece await fonksiyonun asenkron olmasını istiyor onda hata veriyor.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
-  runApp(const MyApp());
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+  demoProjectId: "demo-project-id",
+);
+runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
